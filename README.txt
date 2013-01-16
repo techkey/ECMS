96a560a4da5916681e9c493ec8ea07082e4b30d1
techkey <cornelis_brouwers@hotmail.com>
2013-01-17 06:55:19 +0700

    Added ckeditor module.

--- README.txt
+++
@@ -1,7 +0,0 @@
-Easy CMS system.
-
-A small but fast and powerful CMS system.
-
-The goal is to make it as small and fast as possible.
-
-Documentations is work in progress.
---
+++ modules/ckeditor/ckeditor.class.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * @file ckeditor.class.php
+ */
+
+namespace modules\ckeditor;
+
+use core\modules\module\module;
+
+/**
+ *
+ */
+class ckeditor extends module {
+
+
+  /* Hooks ********************************************************************/
+
+  /**
+   * Hook init().
+   */
+  public function init() {
+    $setting = array(
+      'ckeditor' => array(
+        'enterMode' => variable_get('ckeditor_enter_mode', 'CKEDITOR.ENTER_BR'),
+      ),
+    );
+    add_js($setting, 'setting');
+  }
+
+  /**
+   * Hook menu().
+   *
+   * @return array
+   */
+  public function menu() {
+    $menu['admin/ckeditor'] = array(
+      'title' => 'CKEditor',
+      'controller' => 'ckeditor:settings',
+      'access_arguments' => 'admin',
+      'menu_name' => 'system'
+    );
+
+    return $menu;
+  }
+
+  /* Private routes ***********************************************************/
+
+  /**
+   * @return string
+   */
+  public function settings() {
+    $out = '';
+
+    $form['inline'] = array(
+      '#type' => 'fieldset',
+      '#title' => 'Inline',
+    );
+    $form['inline']['enter_mode'] = array(
+      '#type' => 'radios',
+      '#title' => 'Enter mode',
+      '#description' => 'Sets the behavior of the <em>Enter</em> key.',
+      '#options' => array(1 => 'p', 2 => 'br', 3 => 'div'),
+      '#default_value' => variable_get('ckeditor_enter_mode', 1),
+      '#field_prefix' => '<div>',
+      '#field_suffix' => '</div>',
+    );
+    $form['inline']['buttons'] = array(
+      '#type' => 'checkboxes',
+      '#title' => 'Buttons',
+      '#description' => 'Buttons to show.',
+      '#options' => make_array_assoc(array('htmlSource', 'Bold', 'Italic', 'Underline')),
+    );
+
+    $form['submit'] = array(
+      '#type' => 'submit',
+      '#value' => 'Save',
+    );
+
+    $out .= get_module_form()->build($form);
+
+    return $out;
+  }
+
+  /**
+   * @param array $form
+   * @param array $form_values
+   */
+  public function settings_submit(array $form, array $form_values) {
+//    $enter_modes = array('CKEDITOR.ENTER_P', 'CKEDITOR.ENTER_BR', 'CKEDITOR.ENTER_DIV');
+//    $enter_mode = $enter_modes[$form_values['enter_mode']];
+    variable_set('ckeditor_enter_mode', $form_values['enter_mode']);
+    set_message('Settings saved.');
+  }
+}
