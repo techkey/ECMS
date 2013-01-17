fa8364e288eec0bfcee824119c30a74ab505563e
techkey <cornelis_brouwers@hotmail.com>
2013-01-17 07:49:53 +0700

    Cleaned up.

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <?php
-// index.php
+/**
+ * @file index.php
+ */
 ini_set('error_append_string', '');
 ini_set('error_reporting', -1);
-//ini_set('error_log', '/home/vhosts/www.3dflat.tk/core/log/php_error.log');
-//ini_set('error_log', '/home/a3dfla/domains/3dflat.tk/public_html/core/log/php_error.log');
 
 define('START_TIME', microtime(TRUE));
-
-//exit('Maintenance. We will be back soon!');
 
 require_once __DIR__ . '/core/bootstrap.inc.php';
 
